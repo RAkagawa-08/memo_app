886f1df8a18239d75b7a3c8b48c7b6b556c4a29f
RyunosukeAkagawa <ryunosuke19910601@gmail.com>
2025-01-19 14:00:24 +0900

    一覧機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,9 +7,17 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
+    public function index()
+    {
+        $memos = Memo::all();
+
+        return view('memos.index', ['memos' => $memos]);
+    }
+
     public function show($id)
     {
         $memo = Memo::find($id);
+        
         return view('memos.show', ['memo' => $memo]);
     }
 }
